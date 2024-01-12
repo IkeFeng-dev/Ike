0551167959e75937f798e78fc1fa236417516110
IkeFeng <2063581814@qq.com>
2024-01-12 14:25:32 +0800

    Test

--- Ike/Common.cs
+++ Ike/Common.cs
@@ -11,6 +11,26 @@ namespace Ike
     /// </summary>
     public static class Common
     {
-
+        /// <summary>
+        /// 截取两个字符串之间字符串
+        /// </summary>
+        /// <param name="sourse">文本内容</param>
+        /// <param name="startstr">开始截取的内容</param>
+        /// <param name="endstr">结束截取的内容</param>
+        /// <returns></returns>
+        public static string IntercepString(string sourse, string startstr, string endstr)
+        {
+            string result = string.Empty;
+            int startindex, endindex;
+            startindex = sourse.IndexOf(startstr);
+            if (startindex == -1)
+                return result;
+            string tmpstr = sourse[(startindex + startstr.Length)..];
+            endindex = tmpstr.IndexOf(endstr);
+            if (endindex == -1)
+                return result;
+            result = tmpstr.Remove(endindex);
+            return result;
+        }
     }
 }
